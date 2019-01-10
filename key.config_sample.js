1630fe64911f5ac75064057b7c90641fc9b8929b
Rejun Philip <rejun.philip@gmail.com>
2019-01-10 15:37:46 +0530

    KeySample File

---
+++ key.config_sample.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "MONGODB_ATLAS_URI" : "mongodb+srv://main_user_1:XXXXXXXXXXXX@maincluster-XXXXX.mongodb.net/test?retryWrites=true",
+  "ALPHAVANTAGE_API_KEY" : "XXXXXXXXXXX"
+}
